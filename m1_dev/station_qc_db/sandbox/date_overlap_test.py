79e63d886e6a81051e14d07fd9c2882e082795d8
Greg Fall <Gregory.Fall@noaa.gov>
2020-08-27 11:06:28 -0500

    Testing program for improvements to wdb0.get_air_temp_obs function.

--- m1_dev/station_qc_db/sandbox/date_overlap_test.py
+++ m1_dev/station_qc_db/sandbox/date_overlap_test.py
@@ -1,19 +1,39 @@
 #!/usr/bin/python3
 
 import datetime as dt
+import sys
+import os
 
-time1 = dt.datetime.strptime('2020-01-10 13:00:00 UTC',
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', '..',
+                             'lib'))
+import wdb0
+
+pkl_dir = '/net/scratch/nwm_snow_da/wdb0_pkl'
+
+time1 = dt.datetime.strptime('2020-01-10 11:00:00 UTC',
                              '%Y-%m-%d %H:%M:%S UTC')
-time2 = dt.datetime.strptime('2020-01-10 19:00:00 UTC',
+time2 = dt.datetime.strptime('2020-01-11 11:00:00 UTC',
                              '%Y-%m-%d %H:%M:%S UTC')
 time_range = time2 - time1
 num_hours = time_range.days * 24 + time_range.seconds // 3600 + 1
 prev_obs_datetime = [time1 +
                      dt.timedelta(hours=i) for i in range(num_hours)]
 
+# Get "prior" air temperatures. Okay to get them from a .pkl file.
+current_datetime = dt.datetime.utcnow()
+# wdb_prior_tair = \
+#     wdb0.get_air_temp_obs(time1, time2, verbose=True)
+wdb_prior_tair = \
+    wdb0.get_air_temp_obs(time1, time2, scratch_dir=pkl_dir, verbose=True)
+wdb_prev_tair_datetime = dt.datetime.utcnow()
+elapsed_time = wdb_prev_tair_datetime - current_datetime
+print('INFO: query ran in {} seconds.'.
+      format(elapsed_time.total_seconds()))
+
+
 begin_datetime = dt.datetime.strptime('2020-01-10 12:00:00 UTC',
                                       '%Y-%m-%d %H:%M:%S UTC')
-end_datetime = dt.datetime.strptime('2020-01-10 18:00:00 UTC',
+end_datetime = dt.datetime.strptime('2020-01-11 12:00:00 UTC',
                                     '%Y-%m-%d %H:%M:%S UTC')
 time_range = end_datetime - begin_datetime
 num_hours = time_range.days * 24 + time_range.seconds // 3600 + 1
@@ -25,36 +45,50 @@ prev_end_datetime = prev_obs_datetime[-1]
 
 keep_going = True
 
-both = sorted(list(set(prev_obs_datetime) & set(obs_datetime)))
-if len(both) == 0:
+dt_in_both = sorted(list(set(prev_obs_datetime) & set(obs_datetime)))
+if len(dt_in_both) == 0:
     keep_going = False
 
 if keep_going:
-    obs_in_prev_obs = [obs_datetime.index(dt_in_both) for dt_in_both in both]
+    obs_in_prev_obs = [obs_datetime.index(i) for i in dt_in_both]
     if (obs_in_prev_obs[0] != 0) and (obs_in_prev_obs[-1] != (num_hours - 1)):
         # Multiple SELECT statements (or a more complex select statement)
         # would be required to fill out the data.
         keep_going = False
 
 if keep_going:
-    need = sorted(list(set(obs_datetime) - set(prev_obs_datetime)))
-    print(need)
-    new_data_begin_datetime = need[0]
-    new_data_end_datetime = need[-1]
-
+    dt_needed = sorted(list(set(obs_datetime) - set(prev_obs_datetime)))
+    print(dt_needed)
+    new_data_begin_datetime = dt_needed[0]
+    new_data_end_datetime = dt_needed[-1]
 
 print(keep_going)
 print(obs_in_prev_obs)
 print(new_data_begin_datetime)
 print(new_data_end_datetime)
+print('--')
+
+# Get new temperatures - leave out "scratch_dir=pkl_dir" so the function has
+# to do the work.
+t1 = dt.datetime.utcnow()
+wdb_new_tair = \
+    wdb0.get_air_temp_obs(begin_datetime,
+                          end_datetime,
+                          verbose=True,
+                          prev_obs_air_temp=wdb_prior_tair)
+t2 = dt.datetime.utcnow()
+elapsed_time = t2 - t1
+print('INFO: query ran in {} seconds.'.
+      format(elapsed_time.total_seconds()))
+
 # print([need[i].strftime('%Y%m%d%H') for i in range(len(need))])
 
 # print([prev_obs_datetime[i].strftime('%Y%m%d%H')
 #        for i in range(len(prev_obs_datetime))])
 # print([obs_datetime[i].strftime('%Y%m%d%H')
 #        for i in range(len(obs_datetime))])
-# print([both[i].strftime('%Y%m%d%H')
-#        for i in range(len(both))])
+# print([dt_in_both[i].strftime('%Y%m%d%H')
+#        for i in range(len(dt_in_both))])
 
 if __name__ == 'main':
     main()
