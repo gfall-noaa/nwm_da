daef76a216b4547f235786da79b7e187d4ad061d
Greg Fall <Gregory.Fall@noaa.gov>
2020-08-27 10:50:25 -0500

    Changed function name wdb0.get_snwd_snowfall_obs to wdb0.get_snwd_snfl_obs, updated update_station_qc_db.py to match. Began work on changes to wdb0.get_air_temp_obs to accommodate prev_obs_air_temp keyword (for faster queries).

--- lib/wdb0.py
+++ lib/wdb0.py
@@ -14,7 +14,7 @@ get_prev_snow_depth_obs
 get_prev_swe_obs
 get_air_temp_obs
 get_prev_air_temp_obs
-get_snwd_snowfall_obs
+get_snwd_snfl_obs
 get_snwd_precip_obs
 get_swe_prcp_obs
 get_prv_airtemp_obs
@@ -895,7 +895,8 @@ def get_air_temp_obs(begin_datetime,
                      no_data_value=-99999.0,
                      bounding_box=None,
                      scratch_dir=None,
-                     verbose=None):
+                     verbose=None,
+                     prev_obs_air_temp=None):
 
     """
     Get hourly air temperature observations from the "web_data" database on
@@ -922,9 +923,61 @@ def get_air_temp_obs(begin_datetime,
                 file_obj.close()
                 return(obs_air_temp)
 
+
     time_range = end_datetime - begin_datetime
     num_hours = time_range.days * 24 + time_range.seconds // 3600 + 1
     # print('num_hours = {}'.format(num_hours))
+    obs_datetime = [begin_datetime +
+                    dt.timedelta(hours=i) for i in range(num_hours)]
+
+    if prev_obs_air_temp is not None:
+
+        # Determine whether the prev_obs_air_temp.obs_datetime overlaps with a
+        # portion of either the beginning or the end of obs_datetime.
+        prev_obs_datetime = prev_obs_air_temp['obs_datetime']
+        prev_begin_datetime = prev_obs_datetime[0]
+        prev_end_datetime = prev_obs_datetime[-1]
+
+        dt_in_both = sorted(list(set(prev_obs_datetime) & set(obs_datetime)))
+        # prev_obs_in_obs = [prev_obs_datetime.index(i) for i in dt_in_both]
+
+        # Verify that the list of new datetimes for which we need data is
+        # continguous, which keeps this function simple.
+        # print('obs_datetime:')
+        # print(obs_datetime)
+        # print('dt_in_both:')
+        # print(dt_in_both)
+        obs_dt_from_prev_obs_dt = [obs_datetime.index(i) for i in dt_in_both]
+        print('obs_dt_from_prev_obs_dt:')
+        print(obs_dt_from_prev_obs_dt)
+        if len(obs_dt_from_prev_obs_dt) == 0:
+            prev_obs_air_temp = None
+        else:
+            if (obs_dt_from_prev_obs_dt[0] != 0) and \
+               (obs_dt_from_prev_obs_dt[-1] != (num_hours - 1)):
+                prev_obs_air_temp = None
+
+    if prev_obs_air_temp is not None:
+        # Identify the datetimes for which new data is needed.
+        # print('obs_dt_from_prev_obs_dt:')
+        # print(obs_dt_from_prev_obs_dt)
+        dt_needed = sorted(list(set(obs_datetime) - set(prev_obs_datetime)))
+        # print('dt_needed:')
+        # print(dt_needed)
+        curr_begin_datetime = dt_needed[0]
+        curr_end_datetime = dt_needed[-1]
+        curr_num_hours = len(dt_needed)
+        curr_obs_datetime = [curr_begin_datetime +
+                             dt.timedelta(hours=i)
+                             for i in range(curr_num_hours)]
+        obs_dt_from_curr_obs_dt = [obs_datetime.index(i)
+                                   for i in curr_obs_datetime]
+
+    else:
+        curr_begin_datetime = begin_datetime
+        curr_end_datetime = end_datetime
+        curr_num_hours = num_hours
+        curr_obs_datetime = obs_datetime
 
     # Open the web database.
     conn_string = "host='wdb0.dmz.nohrsc.noaa.gov' dbname='web_data'"
@@ -945,10 +998,10 @@ def get_air_temp_obs(begin_datetime,
               'value AS obs_air_temp_deg_c ' + \
               'FROM point.allstation, point.obs_airtemp ' + \
               'WHERE date >= \'' + \
-              begin_datetime.strftime('%Y-%m-%d %H:%M:%S') + \
+              curr_begin_datetime.strftime('%Y-%m-%d %H:%M:%S') + \
               '\' ' + \
               'AND date <= \'' + \
-              end_datetime.strftime('%Y-%m-%d %H:%M:%S') + \
+              curr_end_datetime.strftime('%Y-%m-%d %H:%M:%S') + \
               '\' ' + \
               'AND point.allstation.obj_identifier = ' + \
               'point.obs_airtemp.obj_identifier ' + \
@@ -999,21 +1052,21 @@ def get_air_temp_obs(begin_datetime,
     station_elevation = []
     station_rec_elevation = []
     # Create a 2-d [time, station] array.
-    obs = np.ma.empty([1, num_hours], dtype=float)
-    obs[0,:] = no_data_value
-    obs[0,:] = np.ma.masked
+    curr_obs = np.ma.empty([1, curr_num_hours], dtype=float)
+    curr_obs[0,:] = no_data_value
+    curr_obs[0,:] = np.ma.masked
     for ind, row in df.iterrows():
         if row['obj_identifier'] != current_station_obj_id:
             station_ind += 1
             if station_ind > 0:
                 # Just finished all data for previous station.
-                obs = np.ma.append(obs,
+                curr_obs = np.ma.append(curr_obs,
                                    np.ma.empty([1,
-                                                num_hours],
+                                                curr_num_hours],
                                                dtype=float),
                                    axis=0)
-                obs[station_ind,:] = no_data_value
-                obs[station_ind,:] = np.ma.masked
+                curr_obs[station_ind,:] = no_data_value
+                curr_obs[station_ind,:] = np.ma.masked
             # New station
             station_obj_id.append(row['obj_identifier'])
             station_id.append(row['station_id'])
@@ -1025,20 +1078,19 @@ def get_air_temp_obs(begin_datetime,
 
             current_station_obj_id = station_obj_id[station_ind]
 
-        # Add the observation to the obs array.
-        time_diff = row['date'] - begin_datetime
+        # Add the observation to the curr_obs array.
+        time_diff = row['date'] - curr_begin_datetime
         time_ind = time_diff.days * 24 + time_diff.seconds // 3600
 
-        obs[station_ind, time_ind] = row['obs_air_temp_deg_c']
+        curr_obs[station_ind, time_ind] = row['obs_air_temp_deg_c']
 
-    num_stations = station_ind + 1
-
-    obs_datetime = [begin_datetime +
-                    dt.timedelta(hours=i) for i in range(num_hours)]
+    curr_num_stations = station_ind + 1
 
     # Place results in a dictionary.
-    obs_air_temp = {'num_stations': num_stations,
-                    'num_hours': num_hours,
+    print('num_hours: {}'.format(num_hours))
+    print('curr_num_hours: {}'.format(curr_num_hours))
+    obs_air_temp = {'num_stations': curr_num_stations,
+                    'num_hours': curr_num_hours,
                     'station_obj_id': station_obj_id,
                     'station_id': station_id,
                     'station_name': station_name,
@@ -1046,8 +1098,8 @@ def get_air_temp_obs(begin_datetime,
                     'station_lat': station_lat,
                     'station_elevation': station_elevation,
                     'station_rec_elevation': station_rec_elevation,
-                    'obs_datetime': obs_datetime,
-                    'values_deg_c': obs}
+                    'obs_datetime': curr_obs_datetime,
+                    'values_deg_c': curr_obs}
 
     # Create the pkl file if all data fetched is more than 60 days earlier
     # than the current date/time.
@@ -1059,6 +1111,57 @@ def get_air_temp_obs(begin_datetime,
         file_obj.close()
         if verbose:
             print('INFO: wrote query results to {}.'.format(file_name))
+
+    if prev_obs_air_temp is not None:
+
+        # Combine prev_obs_air_temp and obs_air_temp.
+        print('# prev stations: {}'.format(prev_obs_air_temp['num_stations']))
+
+        print('# current stations: {}'.format(obs_air_temp['num_stations']))
+        prev_station_obj_id = prev_obs_air_temp['station_obj_id']
+        new_station_obj_id = sorted(list(set(prev_station_obj_id) |
+                                         set(station_obj_id)))
+        print('# combined: {}'.format(len(new_station_obj_id)))
+        # Identify indices of previous and current object identifiers in the
+        # combined list.
+        # Using ".index" stuff is very slow.
+        new_station_from_prev = [new_station_obj_id.index(i)
+                                 for i in prev_station_obj_id]
+        new_station_from_curr = [new_station_obj_id.index(i)
+                                 for i in station_obj_id]
+        # print(len(new_station_from_prev))
+        # print(len(new_station_from_curr))
+        # print(new_station_from_prev[0:50])
+        # print(new_station_from_curr[0:50])
+        new_num_stations = len(new_station_obj_id)
+        prev_obs_in_obs = [prev_obs_datetime.index(i) for i in dt_in_both]
+        prev_obs = prev_obs_air_temp['values_deg_c'][:,prev_obs_in_obs]
+        # print(prev_obs_air_temp['values_deg_c'][:,
+        # print(len(obs_datetime))
+        # print(len(curr_obs_datetime))
+        # print(prev_obs_air_temp['values_deg_c'].shape)
+        # print(prev_obs.shape)
+        # print(curr_obs.shape)
+        # print(prev_obs.shape)
+        # print(len(new_station_from_prev))
+        # print(type(new_station_from_prev))
+        # print(len(obs_dt_from_prev_obs_dt))
+        # print(type(obs_dt_from_prev_obs_dt))
+        print(len(new_station_from_curr))
+        print(obs_dt_from_curr_obs_dt)
+        obs = np.ma.empty([new_num_stations, num_hours], dtype=float)
+        obs[:,:] = no_data_value
+        obs[:,:] = np.ma.masked
+        print(obs.shape)
+        si = 100
+        print(new_station_obj_id[si])
+        print(obs[si,:])
+        obs[np.ix_(new_station_from_prev, obs_dt_from_prev_obs_dt)] = \
+            prev_obs
+        print(obs[si,:])
+        obs[np.ix_(new_station_from_curr, obs_dt_from_curr_obs_dt)] = \
+            curr_obs
+        print(obs[si,:])
 
     return(obs_air_temp)
 
@@ -1263,12 +1366,12 @@ def get_prev_air_temp_obs(target_datetime,
     return(obs_air_temp)
 
 
-def get_snwd_snowfall_obs(target_datetime,
-                          duration_hours,
-                          no_data_value=-99999.0,
-                          bounding_box=None,
-                          scratch_dir=None,
-                          verbose=None):
+def get_snwd_snfl_obs(target_datetime,
+                      duration_hours,
+                      no_data_value=-99999.0,
+                      bounding_box=None,
+                      scratch_dir=None,
+                      verbose=None):
 
     """
     Get snowfall observations from the "web_data" database on wdb0 having
