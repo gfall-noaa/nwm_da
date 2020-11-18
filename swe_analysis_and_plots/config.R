917d6eb4188eebfaa27c4386c0a1b61b7a1f7f1a
Greg Fall <Gregory.Fall@noaa.gov>
2020-11-18 12:38:07 -0600

    Merged with Ziya's config.R.

--- swe_analysis_and_plots/config.R
+++ swe_analysis_and_plots/config.R
@@ -2,3 +2,4 @@
 
 source("/net/home/zzhang/dev/nwm_da/swe_analysis_and_plots/config_western_usa.R")
 #source(" /net/home/zzhang/dev/nwm_da/swe_analysis_and_plots/config_conus.R")
+#source("/net/home/zzhang/dev/nwm_da/swe_analysis_and_plots/config_sierra_nevada_agu.R")
